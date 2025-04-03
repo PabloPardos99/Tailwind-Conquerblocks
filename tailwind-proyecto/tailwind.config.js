57ae08e4d1ca068ac58227fff6c4114095a5aeca
Pablo Pardos <pablo.pardos99@gmail.com>
2025-04-03 14:02:05 +0200

    Subiendo proyecto de portfolio de Tailwind

--- tailwind-proyecto/tailwind.config.js
+++ tailwind-proyecto/tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./index.html', './src/**/'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'hsl(210, 100%, 97%)',
+        foreground: 'hsl(339, 20%, 20%)',
+        primary: 'hsl(308, 56%, 85%)', 
+        secondary: 'hsl(196, 75%, 88%)', 
+        accent: 'hsl(211, 86%, 70%)',
+        destructive: 'hsl(0, 93%, 73%)',
+        muted: 'hsl(210, 100%, 95%)',
+        card: 'hsl(210, 100%, 97%)',
+        border: 'hsl(210, 40%, 80%)',
+      },
+      borderRadius: {
+        default: '1rem',
+      },
+      fontFamily: {
+        sans: ['Manrope', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
